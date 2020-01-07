d4d3dfc86aaf738aee6c2ed339d3bd2b756fc244
Paulo Flabiano Smorigo <pfsmorigo@gmail.com>
2020-01-07 20:06:59 -0300

    bltouch is at the oposite side
    Signed-off-by: Paulo Flabiano Smorigo <pfsmorigo@gmail.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -931,7 +931,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 48, -2, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
